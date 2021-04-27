7e1815edcb01c1aa2a9624cc7b7ee4e44f5bd130
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-27 12:31:09 -0500

    Remove comments from invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,21 +11,4 @@ class Invoice < ApplicationRecord
   enum status: [:"in progress", :completed, :cancelled]
 
   scope :incomplete_invoices, -> { includes(:invoice_items).where.not(status: 2).distinct.order(:created_at)}
-
-
-  # def discounted_rev
-  #   invoice_items.
-  #   joins(:bulk_discounts).
-  #   where('bulk_discounts.minimum_quantity <= invoice_items.quantity').
-  #   select('bulk_discounts.*', 'invoice_items.*', ('invoice_items.unit_price * bulk_discounts.percent_discount') as discounted_revenue).
-  #   order('bulk_discounts.percent_discount DESC')
-  # end
-
-  # def discounted_revenue
-  #   discount = discounted_rev.uniq.sum(&:discounted_revenue)
-  #   invoice_items.total_revenue - discount
-  # end
-
-  #############################################################################
-  # invoice_items.joins(:bulk_discounts).select('bulk_discounts.*', 'invoice_items.*').order('bulk_discounts.percentage DESC').percentage
 end
